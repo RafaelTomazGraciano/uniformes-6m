57def07b56e68439ee641af285b4f674bec2999a
Rafael Tomaz Graciano <rafaeltomaz2005@gmail.com>
2026-06-21 15:17:37 -0300

    feat: repositories

---
+++ backend/uniform/src/main/java/com/six_m/uniform/domain/aluno/AlunoRepository.java
@@ -0,0 +1,11 @@
+package com.six_m.uniform.domain.aluno;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, UUID> {
+
+}
---
+++ backend/uniform/src/main/java/com/six_m/uniform/domain/notaFiscal/NotaFiscalRepository.java
@@ -0,0 +1,10 @@
+package com.six_m.uniform.domain.notaFiscal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface NotaFiscalRepository extends JpaRepository<NotaFiscal, UUID> {
+}
