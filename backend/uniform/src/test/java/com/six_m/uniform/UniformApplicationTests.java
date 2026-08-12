56337956c0e34a1b81f6ee511422a577a66fcd74
Rafael Tomaz Graciano <124164062+RafaelTomazGraciano@users.noreply.github.com>
2026-08-11 21:15:16 -0300

    Revert "feat: unit tests"
    This reverts commit 2110cc1d4cf5e563febcbb14206d0ea43a54aca8.

---
+++ backend/uniform/src/test/java/com/six_m/uniform/UniformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.six_m.uniform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
