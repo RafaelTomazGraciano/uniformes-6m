0652e3f0395fc78f9abf723ffafbcb756e0c8f8b
Rafael Tomaz Graciano <124164062+RafaelTomazGraciano@users.noreply.github.com>
2026-08-11 21:15:16 -0300

    Revert "feat: unit tests"
    This reverts commit 2110cc1d4cf5e563febcbb14206d0ea43a54aca8.

--- backend/uniform/src/main/java/com/six_m/uniform/domain/usuario/UsuarioRepository.java
+++ backend/uniform/src/main/java/com/six_m/uniform/domain/usuario/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.six_m.uniform.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
