8cc1e1c39495e74c8793917e6f7f17c5346376f7
Gabriel Witor <witor.alm@gmail.com>
2026-08-27 07:54:51 -0300

    chore: update cors allowed origins

--- backend/src/main/java/com/six_m/uniform/config/SecurityConfig.java
+++ backend/src/main/java/com/six_m/uniform/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(allowedOrigins);
+        config.setAllowedOriginPatterns(allowedOrigins);
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("*"));
 
